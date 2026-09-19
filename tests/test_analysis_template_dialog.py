55f82a5be37512b03466f173ca4225d90558e931
Tran-Van Han <57890472+tranhan1405@users.noreply.github.com>
2026-09-19 15:37:19 +0200

    test(ui): use 2D fixity width

--- tests/test_analysis_template_dialog.py
+++ tests/test_analysis_template_dialog.py
@@ -102,7 +102,7 @@ def test_pushover_dialog_converts_roof_drift_to_displacement(qapp):
     model.add_node(1, 0.0, 0.0, 0.0)
     model.add_node(2, 0.0, 3.0, 0.0)
     model.add_node(3, 0.0, 6.0, 0.0)
-    model.set_fixity(1, (1, 1, 1, 1, 1, 1))
+    model.set_fixity(1, (1, 1, 1))
     project = ProjectDatabase(model=model)
 
     dialog = AnalysisTemplateDialog(
